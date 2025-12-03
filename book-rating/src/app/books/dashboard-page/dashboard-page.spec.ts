04fbe7137101c3025c0dbba7f2b9298e8f29bc99
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-12-03 22:23:46 +0100

    vi.spyOn ohne den ollen "bookRatingMock" - einfach Spy drauf und gut! FYI @fmalcher

--- book-rating/src/app/books/dashboard-page/dashboard-page.spec.ts
+++ book-rating/src/app/books/dashboard-page/dashboard-page.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { vi } from 'vitest';
 
 import { DashboardPage } from './dashboard-page';
+import { BookRatingHelper } from '../shared/book-rating-helper';
 
 describe('DashboardPage', () => {
   let component: DashboardPage;
@@ -17,7 +19,20 @@ describe('DashboardPage', () => {
     await fixture.whenStable();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should use BookRatingHelper.rateUp() to rate up a book', () => {
+
+    const testBook = {
+      isbn: '000',
+      title: 'Angular',
+      description: 'Tolles Buch',
+      rating: 3
+    };
+
+    const bookRatingHelper = TestBed.inject(BookRatingHelper);
+    const spy = vi.spyOn(bookRatingHelper, 'rateUp');
+
+    component.doRateUp(testBook);
+
+    expect(spy).toHaveBeenCalledExactlyOnceWith(testBook);
   });
 });
