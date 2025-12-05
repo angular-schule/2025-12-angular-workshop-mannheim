bbbee7e7f5e107443cd589fc7ee2527f8c49cc4b
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-12-05 16:15:14 +0100

    rxjs: mergeMap

--- book-rating/src/app/books/book-details-page/book-details-page.ts
+++ book-rating/src/app/books/book-details-page/book-details-page.ts
@@ -1,31 +1,41 @@
 import { Component, effect, inject, input, signal } from '@angular/core';
 import { BookStore } from '../shared/book-store';
 import { Book } from '../shared/book';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { JsonPipe } from '@angular/common';
+import { map, mergeMap } from 'rxjs';
 
 @Component({
   selector: 'app-book-details-page',
-  imports: [RouterLink],
+  imports: [RouterLink, JsonPipe],
   templateUrl: './book-details-page.html',
   styleUrl: './book-details-page.scss',
 })
 export class BookDetailsPage {
 
-  isbn = input.required<string>();
-
+  router = inject(ActivatedRoute);
   bookStore = inject(BookStore);
-  book = signal<Book | undefined>(undefined);
+
+
+  book = toSignal(this.router.paramMap.pipe(
+    map(paramMap => paramMap.get('isbn') || ''),
+    mergeMap(isbn => this.bookStore.getSingleBook(isbn))
+  ))
+
+
+  // isbn = input.required<string>();
 
   constructor() {
 
     // synchron ok
     // effect(() => console.log('Die ISBN lautet', this.isbn()))
 
-    effect(() => {
-      const isbn = this.isbn();
-      this.bookStore.getSingleBook(isbn)
-        .subscribe(b => this.book.set(b))
-    })
+    // effect(() => {
+    //   const isbn = this.isbn();
+    //   this.bookStore.getSingleBook(isbn)
+    //     .subscribe(b => this.book.set(b))
+    // })
 
   }
 
