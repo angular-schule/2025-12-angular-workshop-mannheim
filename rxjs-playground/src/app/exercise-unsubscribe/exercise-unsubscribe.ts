16b457fb0da6b0e3cfd86dd70e11a4cf4fe07c99
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-12-05 15:53:19 +0100

    rxjs: takeUntilDestroyed

--- rxjs-playground/src/app/exercise-unsubscribe/exercise-unsubscribe.ts
+++ rxjs-playground/src/app/exercise-unsubscribe/exercise-unsubscribe.ts
@@ -8,18 +8,19 @@ import { HistoryWindow } from '../shared/history-window/history-window';
   templateUrl: './exercise-unsubscribe.html',
   imports: [HistoryWindow]
 })
-export class ExerciseUnsubscribe implements OnDestroy {
+export class ExerciseUnsubscribe {
 
   logStream$ = new ReplaySubject<unknown>();
+
 
   /**
    * Öffne die Browser-Console: Dort siehst Du den Output eines Observables, das jede Sekunde einen Wert generiert.
    * Navigiere zurück auf die Startseite und beobachte die Console:
    * Die Subscription läuft weiter. Wir haben einen klassischen Memory Leak erzeugt ...
-   * 
+   *
    * Sorge dafür, dass die Subscription in der Methode ngOnDestroy() beendet wird!
    * Sie wird beim Buttonklick und beim Wegnavigieren ausgelöst.
-   * 
+   *
    * Es gibt noch weitere Wege, das Problem zu lösen ...
    */
   constructor() {
@@ -27,19 +28,13 @@ export class ExerciseUnsubscribe implements OnDestroy {
 
     interval$.pipe(
 
-      /******************************/
-
-      
-      /******************************/
+      takeUntilDestroyed()
 
     ).subscribe({
       next: e => this.log(e),
       error: err => this.log('❌ ERROR: ' + err),
       complete: () => this.log('✅ COMPLETE')
     });
-  }
-
-  ngOnDestroy() {
   }
 
   log(msg: unknown) {
