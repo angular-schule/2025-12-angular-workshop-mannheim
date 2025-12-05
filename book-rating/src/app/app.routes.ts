e36659a559deaee61d5c23ac565f71091f027fee
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-12-05 10:50:01 +0100

    routing

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { DashboardPage } from './books/dashboard-page/dashboard-page';
+import { BookDetailsPage } from './books/book-details-page/book-details-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'books', component: DashboardPage, title: 'Dashboard' },
+  { path: 'books/:isbn', component: BookDetailsPage, title: 'Details' },
+  { path: '', pathMatch: 'full', redirectTo: 'books' }
+
+];
+
+
+// http://localhost:4200/
+// http://localhost:4200/books/12345
