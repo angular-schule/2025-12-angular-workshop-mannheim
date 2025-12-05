92c515e4dca0171983fcd27c60766306cd06d5fb
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-12-05 12:46:44 +0100

    HTTP antworten sind nicht deterministisch

--- book-rating/src/app/books/book-details-page/book-details-page.ts
+++ book-rating/src/app/books/book-details-page/book-details-page.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, input, signal } from '@angular/core';
+import { BookStore } from '../shared/book-store';
+import { Book } from '../shared/book';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-book-details-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './book-details-page.html',
   styleUrl: './book-details-page.scss',
 })
 export class BookDetailsPage {
+
+  isbn = input.required<string>();
+
+  bookStore = inject(BookStore);
+  book = signal<Book | undefined>(undefined);
+
+  constructor() {
+
+    // synchron ok
+    // effect(() => console.log('Die ISBN lautet', this.isbn()))
+
+    effect(() => {
+      const isbn = this.isbn();
+      this.bookStore.getSingleBook(isbn)
+        .subscribe(b => this.book.set(b))
+    })
+
+  }
 
 }
